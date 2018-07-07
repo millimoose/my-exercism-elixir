72fd7618121f4e949c9789733c1e7b14e849275f
David Vallner <millimoose@users.noreply.github.com>
2018-07-08 01:58:32 +0200

    add secret handshake solution

--- secret-handshake/secret_handshake.exs
+++ secret-handshake/secret_handshake.exs
@@ -1,4 +1,18 @@
 defmodule SecretHandshake do
+  use Bitwise, only_operators: true
+
+  @actions [
+    {0b1, "wink"},
+    {0b10, "double blink"},
+    {0b100, "close your eyes"},
+    {0b1000, "jump"}
+  ]
+
+  # Return true if the code matches the given bit pattern; i.e. if all the bits
+  # in the pattern are set in the code.
+  @spec is_set(code :: integer, bits :: integer) :: integer
+  defp is_set(code, bits), do: (code &&& bits) === bits
+
   @doc """
   Determine the actions of a secret handshake based on the binary
   representation of the given `code`.
@@ -15,5 +29,12 @@ defmodule SecretHandshake do
   """
   @spec commands(code :: integer) :: list(String.t())
   def commands(code) do
+    result = for {bits, action} <- @actions, is_set(code, bits), do: action
+
+    if is_set(code, 0b10000) do
+      Enum.reverse(result)
+    else
+      result
+    end
   end
 end
