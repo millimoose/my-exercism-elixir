c250c0939b7f708c26fe002f50140a77835b9cc3
David Vallner <millimoose@users.noreply.github.com>
2018-07-10 03:41:02 +0200

    solve protein translation

---
+++ protein-translation/protein_translation.exs
@@ -0,0 +1,72 @@
+defmodule ProteinTranslation do
+  @doc """
+  Given an RNA string, return a list of proteins specified by codons, in order.
+  """
+  @spec of_rna(String.t()) :: {atom, list(String.t())}
+  def of_rna(rna) do
+    codons = for codon_chars <- Enum.chunk_every(String.to_charlist(rna), 3), do: List.to_string(codon_chars)
+    case of_rna(codons, []) do
+      {:error, message} -> {:error, message}
+      proteins when is_list(proteins) -> {:ok, Enum.reverse(proteins)}
+      _ -> raise "huh"
+    end
+  end
+
+  @spec of_rna([String.t()], [String.t()]) :: {:error, String.t()} | [String.t()]
+  defp of_rna([codon | codons], proteins) do
+    case of_codon(codon) do
+      {:ok, "STOP"} -> proteins
+      {:ok, protein} -> of_rna(codons, [protein|proteins])
+      {:error, _message} -> {:error, "invalid RNA"}
+    end
+  end
+
+  defp of_rna([], proteins) do
+    proteins
+  end
+
+  @doc """
+  Given a codon, return the corresponding protein
+
+  UGU -> Cysteine
+  UGC -> Cysteine
+  UUA -> Leucine
+  UUG -> Leucine
+  AUG -> Methionine
+  UUU -> Phenylalanine
+  UUC -> Phenylalanine
+  UCU -> Serine
+  UCC -> Serine
+  UCA -> Serine
+  UCG -> Serine
+  UGG -> Tryptophan
+  UAU -> Tyrosine
+  UAC -> Tyrosine
+  UAA -> STOP
+  UAG -> STOP
+  UGA -> STOP
+  """
+  @spec of_codon(String.t()) :: {atom, String.t()}
+  def of_codon(codon) do
+    case codon do
+      "UGU" -> {:ok, "Cysteine"}
+      "UGC" -> {:ok, "Cysteine"}
+      "UUA" -> {:ok, "Leucine"}
+      "UUG" -> {:ok, "Leucine"}
+      "AUG" -> {:ok, "Methionine"}
+      "UUU" -> {:ok, "Phenylalanine"}
+      "UUC" -> {:ok, "Phenylalanine"}
+      "UCU" -> {:ok, "Serine"}
+      "UCC" -> {:ok, "Serine"}
+      "UCA" -> {:ok, "Serine"}
+      "UCG" -> {:ok, "Serine"}
+      "UGG" -> {:ok, "Tryptophan"}
+      "UAU" -> {:ok, "Tyrosine"}
+      "UAC" -> {:ok, "Tyrosine"}
+      "UAA" -> {:ok, "STOP"}
+      "UAG" -> {:ok, "STOP"}
+      "UGA" -> {:ok, "STOP"}
+      _ -> {:error, "invalid codon"}
+    end
+  end
+end
