6262dbc64ca4b452606cda823cdc6efe0f4dff91
David Vallner <david@vallner.net>
2018-09-01 03:05:42 +0200

    Solution for Space Age

---
+++ space-age/space_age.exs
@@ -0,0 +1,35 @@
+defmodule SpaceAge do
+  # length of year on Earth
+  @earth_seconds 31_557_600
+
+  @planet_years %{
+    earth: 1.0,
+    mercury: 0.2408467,
+    venus: 0.61519726,
+    mars: 1.8808158,
+    jupiter: 11.862615,
+    saturn: 29.447498,
+    uranus: 84.016846,
+    neptune: 164.79132
+  }
+
+  @type planet ::
+          :mercury
+          | :venus
+          | :earth
+          | :mars
+          | :jupiter
+          | :saturn
+          | :uranus
+          | :neptune
+
+  @doc """
+  Return the number of years a person that has lived for 'seconds' seconds is
+  aged on 'planet'.
+  """
+  @spec age_on(planet, pos_integer) :: float
+  def age_on(planet, seconds) do
+    earth_years = seconds / @earth_seconds
+    earth_years *  @planet_years[:earth] / @planet_years[planet]
+  end
+end
